fb9d8c3b6851454a4f460f0f045bef094993c28b
Austin Beck <beck.a@outlook.com>
2021-01-15 11:31:39 -0500

    Initial Commit

---
+++ Password Logger/Password_LoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_LoggerApp.swift
+//  Password Logger
+//
+//  Created by Austin Beck on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Password_LoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
